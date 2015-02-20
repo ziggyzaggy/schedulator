ce552f02bd8908db7d7b7f42ac29f77475f26047
ziggyzaggy <meres87@gmail.com>
2015-02-20 14:28:52 +0000

    Replaced standard (and boring) android progress bar with smoothprogress bar.

--- app/build/generated/source/r/debug/com/examples/android/calendar/R.java
+++ app/build/generated/source/r/debug/com/examples/android/calendar/R.java
@@ -18,16 +18,82 @@ public final class R {
         public static final int from_left_lesser=0x7f040006;
         public static final int from_right=0x7f040007;
     }
+    public static final class array {
+        public static final int circularPrgBarColors=0x7f050000;
+    }
     public static final class attr {
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int cpbStyle=0x7f010000;
+        /** <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int cpb_color=0x7f010001;
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int cpb_colors=0x7f010002;
+        /** <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int cpb_max_sweep_angle=0x7f010005;
+        /** <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int cpb_min_sweep_angle=0x7f010004;
+        /** <p>Must be a floating point value, such as "<code>1.2</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int cpb_rotation_speed=0x7f010007;
+        /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int cpb_stroke_width=0x7f010003;
+        /** <p>Must be a floating point value, such as "<code>1.2</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int cpb_sweep_speed=0x7f010006;
     }
     public static final class color {
-        public static final int friendsDrawerColor=0x7f050000;
-        public static final int mainBlue=0x7f050001;
-        public static final int mainGrayText=0x7f050002;
-        public static final int mainGreen=0x7f050003;
-        public static final int mainRed=0x7f050004;
-        public static final int text_color=0x7f050005;
-        public static final int white=0x7f050006;
+        public static final int cpb_default_color=0x7f060000;
+        public static final int friendsDrawerColor=0x7f060001;
+        public static final int mainBlue=0x7f060002;
+        public static final int mainGrayText=0x7f060003;
+        public static final int mainGreen=0x7f060004;
+        public static final int mainRed=0x7f060005;
+        public static final int text_color=0x7f060006;
+        public static final int white=0x7f060007;
+    }
+    public static final class dimen {
+        public static final int cpb_default_stroke_width=0x7f070000;
     }
     public static final class drawable {
         public static final int back=0x7f020000;
@@ -50,37 +116,41 @@ public final class R {
         public static final int row_background_selector=0x7f020011;
     }
     public static final class id {
-        public static final int TAG_EVENT_ID=0x7f060000;
-        public static final int avatarTV=0x7f060019;
-        public static final int bind=0x7f060013;
-        public static final int blueIndicator=0x7f060017;
-        public static final int circle_friends=0x7f060007;
-        public static final int circle_green=0x7f060006;
-        public static final int circle_red=0x7f060005;
-        public static final int date=0x7f060014;
-        public static final int dateDetailContainerText=0x7f060009;
-        public static final int dateHeaderText=0x7f06001c;
-        public static final int datePicker1=0x7f06001d;
-        public static final int drawerFriendContainer=0x7f060010;
-        public static final int friendsTV=0x7f060012;
-        public static final int friends_drawer=0x7f060001;
-        public static final int greenIndicator=0x7f060016;
-        public static final int gridview=0x7f06000b;
-        public static final int groupsContainer=0x7f06000d;
-        public static final int groupsTV=0x7f06000e;
-        public static final int indicatorContainer=0x7f060015;
-        public static final int lldrawercontent=0x7f06000c;
-        public static final int mainLayoutContainer=0x7f060002;
-        public static final int nameTV=0x7f06001a;
-        public static final int openButton=0x7f06001e;
-        public static final int prgBarDates=0x7f060008;
-        public static final int redIndicator=0x7f060018;
-        public static final int right_drawer=0x7f060011;
-        public static final int right_drawer_groups=0x7f06000f;
-        public static final int title=0x7f06000a;
-        public static final int topBarWrapper=0x7f060003;
-        public static final int txtGroupName=0x7f06001b;
-        public static final int weekText=0x7f060004;
+        public static final int TAG_EVENT_ID=0x7f080000;
+        public static final int avatarTV=0x7f080019;
+        public static final int bind=0x7f080013;
+        public static final int blueIndicator=0x7f080017;
+        public static final int circle_friends=0x7f080007;
+        public static final int circle_green=0x7f080006;
+        public static final int circle_red=0x7f080005;
+        public static final int date=0x7f080014;
+        public static final int dateDetailContainerText=0x7f080009;
+        public static final int dateHeaderText=0x7f08001c;
+        public static final int datePicker1=0x7f08001d;
+        public static final int drawerFriendContainer=0x7f080010;
+        public static final int friendsTV=0x7f080012;
+        public static final int friends_drawer=0x7f080001;
+        public static final int greenIndicator=0x7f080016;
+        public static final int gridview=0x7f08000b;
+        public static final int groupsContainer=0x7f08000d;
+        public static final int groupsTV=0x7f08000e;
+        public static final int indicatorContainer=0x7f080015;
+        public static final int lldrawercontent=0x7f08000c;
+        public static final int mainLayoutContainer=0x7f080002;
+        public static final int nameTV=0x7f08001a;
+        public static final int openButton=0x7f08001e;
+        public static final int prgBarDates=0x7f080008;
+        public static final int redIndicator=0x7f080018;
+        public static final int right_drawer=0x7f080011;
+        public static final int right_drawer_groups=0x7f08000f;
+        public static final int title=0x7f08000a;
+        public static final int topBarWrapper=0x7f080003;
+        public static final int txtGroupName=0x7f08001b;
+        public static final int weekText=0x7f080004;
+    }
+    public static final class integer {
+        public static final int cpb_default_max_sweep_angle=0x7f090000;
+        public static final int cpb_default_min_sweep_angle=0x7f090001;
     }
     public static final class layout {
         public static final int calendar=0x7f030000;
@@ -91,13 +161,154 @@ public final class R {
         public static final int main=0x7f030005;
     }
     public static final class string {
-        public static final int app_name=0x7f070000;
-        public static final int drawer_close_desc=0x7f070001;
-        public static final int drawer_open_desc=0x7f070002;
-        public static final int hello=0x7f070003;
-        public static final int open=0x7f070004;
+        public static final int app_name=0x7f0a0000;
+        public static final int cpb_default_rotation_speed=0x7f0a0001;
+        public static final int cpb_default_sweep_speed=0x7f0a0002;
+        public static final int drawer_close_desc=0x7f0a0003;
+        public static final int drawer_open_desc=0x7f0a0004;
+        public static final int hello=0x7f0a0005;
+        public static final int open=0x7f0a0006;
     }
     public static final class style {
-        public static final int Theme_NoBackground=0x7f080000;
+        public static final int CircularProgressBar=0x7f0b0000;
+        public static final int Theme_NoBackground=0x7f0b0001;
+        public static final int Theme_SmoothProgressBarDefaults=0x7f0b0002;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a CircularProgressBar.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #CircularProgressBar_cpbStyle com.examples.android.calendar:cpbStyle}</code></td><td></td></tr>
+           <tr><td><code>{@link #CircularProgressBar_cpb_color com.examples.android.calendar:cpb_color}</code></td><td></td></tr>
+           <tr><td><code>{@link #CircularProgressBar_cpb_colors com.examples.android.calendar:cpb_colors}</code></td><td></td></tr>
+           <tr><td><code>{@link #CircularProgressBar_cpb_max_sweep_angle com.examples.android.calendar:cpb_max_sweep_angle}</code></td><td></td></tr>
+           <tr><td><code>{@link #CircularProgressBar_cpb_min_sweep_angle com.examples.android.calendar:cpb_min_sweep_angle}</code></td><td></td></tr>
+           <tr><td><code>{@link #CircularProgressBar_cpb_rotation_speed com.examples.android.calendar:cpb_rotation_speed}</code></td><td></td></tr>
+           <tr><td><code>{@link #CircularProgressBar_cpb_stroke_width com.examples.android.calendar:cpb_stroke_width}</code></td><td></td></tr>
+           <tr><td><code>{@link #CircularProgressBar_cpb_sweep_speed com.examples.android.calendar:cpb_sweep_speed}</code></td><td></td></tr>
+           </table>
+           @see #CircularProgressBar_cpbStyle
+           @see #CircularProgressBar_cpb_color
+           @see #CircularProgressBar_cpb_colors
+           @see #CircularProgressBar_cpb_max_sweep_angle
+           @see #CircularProgressBar_cpb_min_sweep_angle
+           @see #CircularProgressBar_cpb_rotation_speed
+           @see #CircularProgressBar_cpb_stroke_width
+           @see #CircularProgressBar_cpb_sweep_speed
+         */
+        public static final int[] CircularProgressBar = {
+            0x7f010000, 0x7f010001, 0x7f010002, 0x7f010003,
+            0x7f010004, 0x7f010005, 0x7f010006, 0x7f010007
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.examples.android.calendar.R.attr#cpbStyle}
+          attribute's value can be found in the {@link #CircularProgressBar} array.
+
+
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name com.examples.android.calendar:cpbStyle
+        */
+        public static final int CircularProgressBar_cpbStyle = 0;
+        /**
+          <p>This symbol is the offset where the {@link com.examples.android.calendar.R.attr#cpb_color}
+          attribute's value can be found in the {@link #CircularProgressBar} array.
+
+
+          <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.examples.android.calendar:cpb_color
+        */
+        public static final int CircularProgressBar_cpb_color = 1;
+        /**
+          <p>This symbol is the offset where the {@link com.examples.android.calendar.R.attr#cpb_colors}
+          attribute's value can be found in the {@link #CircularProgressBar} array.
+
+
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name com.examples.android.calendar:cpb_colors
+        */
+        public static final int CircularProgressBar_cpb_colors = 2;
+        /**
+          <p>This symbol is the offset where the {@link com.examples.android.calendar.R.attr#cpb_max_sweep_angle}
+          attribute's value can be found in the {@link #CircularProgressBar} array.
+
+
+          <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.examples.android.calendar:cpb_max_sweep_angle
+        */
+        public static final int CircularProgressBar_cpb_max_sweep_angle = 5;
+        /**
+          <p>This symbol is the offset where the {@link com.examples.android.calendar.R.attr#cpb_min_sweep_angle}
+          attribute's value can be found in the {@link #CircularProgressBar} array.
+
+
+          <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.examples.android.calendar:cpb_min_sweep_angle
+        */
+        public static final int CircularProgressBar_cpb_min_sweep_angle = 4;
+        /**
+          <p>This symbol is the offset where the {@link com.examples.android.calendar.R.attr#cpb_rotation_speed}
+          attribute's value can be found in the {@link #CircularProgressBar} array.
+
+
+          <p>Must be a floating point value, such as "<code>1.2</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.examples.android.calendar:cpb_rotation_speed
+        */
+        public static final int CircularProgressBar_cpb_rotation_speed = 7;
+        /**
+          <p>This symbol is the offset where the {@link com.examples.android.calendar.R.attr#cpb_stroke_width}
+          attribute's value can be found in the {@link #CircularProgressBar} array.
+
+
+          <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.examples.android.calendar:cpb_stroke_width
+        */
+        public static final int CircularProgressBar_cpb_stroke_width = 3;
+        /**
+          <p>This symbol is the offset where the {@link com.examples.android.calendar.R.attr#cpb_sweep_speed}
+          attribute's value can be found in the {@link #CircularProgressBar} array.
+
+
+          <p>Must be a floating point value, such as "<code>1.2</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.examples.android.calendar:cpb_sweep_speed
+        */
+        public static final int CircularProgressBar_cpb_sweep_speed = 6;
+    };
 }
